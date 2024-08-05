b345a59d36aafe0ebd884276d29237319a1101b3
KHAL33L <109513724+KHAL33L@users.noreply.github.com>
2024-08-05 15:22:01 +0000

    Update app_date.py

--- app_date.py
+++ app_date.py
@@ -6,17 +6,17 @@ import joblib
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-# Load the models and scaler
+# models and scaler
 lstm_model = joblib.load('lstm_model.keras')
 gru_model = joblib.load('gru_model.keras')
 scaler = joblib.load('scaler.pkl')
 
-# Load the dataset
+# dataset
 df = pd.read_csv("/content/drive/MyDrive/AI Final Project/CFC_traded_sahres_2019_to_date.csv")
 df['Daily Date'] = pd.to_datetime(df['Daily Date'], format='%d/%m/%Y')
 df = df.sort_values('Daily Date')
 
-# Function to create sequences
+# sequences function
 def create_sequences(data, seq_length):
     xs = []
     for i in range(len(data) - seq_length + 1):
@@ -24,21 +24,21 @@ def create_sequences(data, seq_length):
         xs.append(x)
     return np.array(xs)
 
-# Function to make predictions
+# predictions function
 def make_prediction(date, model):
-    # Find the index of the date in the dataframe
+    # find the index of the date in the dataframe
     date_index = df[df['Daily Date'] == date].index[0]
     
-    # Get the previous 30 days of data
+    # get the previous 30 days of data
     data = df.iloc[date_index-29:date_index+1]
     
-    # Scale the data
+    # scaling
     scaled_data = scaler.transform(data.drop(columns=['Daily Date']))
     
-    # Create sequence
+    # creating sequence
     X = create_sequences(scaled_data, 30)
     
-    # Make prediction
+    # prediction
     prediction = model.predict(X)
     
     # Inverse transform the prediction
@@ -53,29 +53,29 @@ st.title('Stock Price Prediction App')
 date = st.date_input('Select a date')
 
     
-    # Check if date is it in the dataset
+    # check if date is in the dataset
 if date not in df['Daily Date'].values:
     st.error('Selected date is not in the dataset. Please choose another date.')
 else:
-    # Make predictions
+    # predictions
     lstm_pred = make_prediction(date, lstm_model)
     gru_pred = make_prediction(date, gru_model)
         
-    # Calculate average prediction
+    # calculating average prediction
     avg_pred = (lstm_pred + gru_pred) / 2
         
-    # Obtain the actual price at the time period
+    # getting the actual price at the time window
     actual_price = df[df['Daily Date'] == date]['Closing Price - VWAP (GH¢)'].values[0]
         
-    # Calculate difference between the average predicted price and the actual price
+    # calculating the difference between the average predicted price and the actual price
     difference = avg_pred - actual_price
         
-    # Display the results
+    # displaying the results
     st.write(f'Predicted Closing Price: GH¢ {avg_pred:.2f}')
     st.write(f'Actual Closing Price: GH¢ {actual_price:.2f}')
     st.write(f'Difference: GH¢ {difference:.2f}')
     
-    # Plotting the graph
+    # graph plotting
     start_date = date - timedelta(days=14)
     end_date = date + timedelta(days=14)
         
